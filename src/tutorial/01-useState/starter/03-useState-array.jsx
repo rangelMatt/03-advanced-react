83da6aa0bad9b1bafce97e52f7d613a92b674dac
Matt Rangel <mattrangel@gmail.com>
2023-03-09 21:15:14 -0800

    coded useState to clear all items and remove single items

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,42 @@
+import { data } from "../../../data";
+import React, { useState } from "react";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+
+  const removeItem = (id) => {
+    console.log(id);
+    const newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+  const clearAllItems = () => {
+    setPeople([]);
+  };
+
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person;
+        // console.log(person);
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+            <button type="button" onClick={() => removeItem(id)}>
+              remove
+            </button>
+          </div>
+        );
+      })}
+      <button
+        type="button"
+        style={{ marginTop: "2rem" }}
+        className="btn"
+        onClick={() => setPeople([])}
+      >
+        clear items
+      </button>
+    </div>
+  );
 };
 
 export default UseStateArray;
