44984657cacdc8d251ea1b71b0ee0b6a31867439
Matt Rangel <mattrangel@gmail.com>
2023-03-15 14:22:41 -0700

    compuleted useState lecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Starter from "./tutorial/01-useState/starter/03-useState-array";
+import Starter from "./tutorial/01-useState/starter/05-useState-gotcha";
+// import Final from "./tutorial/01-useState/starter/04-useState-object";
 
 function App() {
   return (
     <div className="container">
       <Starter />
+      {/* <Final /> */}
     </div>
   );
 }
